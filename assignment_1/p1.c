478a283771cc83f6dc28f915ff4b5c4e87aa70a0
ShijimaSim <signorbarilla@gmail.com>
2025-05-16 09:51:21 +0200

    Completed Exercise 1
    Completed Exercise 1 with regard to all requirements and conventions
    
    Completion of Extra Credit for Exrrcise 1 to be deternined

--- assignment_1/p1.c
+++ assignment_1/p1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
-    printf("hello world from process %d\n", getpid());
+    for(int i = 0; i < 100; ++i) {
+        printf("%d ", i);
+        if (i % 3 == 0) {
+            printf("fizz");
+        }
+        if (i % 5 == 0) {
+            printf("buzz");
+        }
+        printf("\n");
+    }
 }
